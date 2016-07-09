e26b0558806637ace18260979cca7cfe2dd3397c
Travis Paul <Tr@visPaul.me>
2016-07-09 12:22:06 -0400

    Add the ability to supply a request payload from a lua file such as this: return {     env = {         request_uri = "/webhook/github",         request_method = "POST",     },     headers = {},     query = {         payload = '{"repository":{"name":"webhook"}}'     }, }

--- httpd.lua
+++ httpd.lua
@@ -1,34 +1,52 @@
 -- httpd shim for testing
 -- example usage:
---   REQUEST_URI=/greetings/index lua example/greetings/greetings.lua
+--   REQUEST_URI=/webhook/github lua ../src/webhook.lua
+
 local httpd = {}
 	
 httpd.write = function (str)
-    io.write(str)
+  io.write(str)
+end
+
+httpd.print = function (str)
+  io.write(str .. "\r\n")
 end
 
 httpd.register_handler = function (handler, fn)
-    local request_uri = os.getenv('REQUEST_URI')
-    local parts = {}
-    local count = 0
-    if (request_uri == nil) then
-        httpd.write("HTTP/1.1 500 Internal Server Error\r\n") 
-        httpd.write("Content-Type: text/plain\r\n") 
-        httpd.write("Content-Length: 23\r\n\r\n") 
-        httpd.write("No REQUEST_URI provided") 
-        return 1
-    end
-    for word in string.gmatch(request_uri, '([^/]+)') do
-        parts[count] = word
-        count = count + 1
-    end
-    if (parts[1] == handler) then 
-        return fn({},{},{})
-    end
-    httpd.write("HTTP/1.1 404 Not Found\r\n") 
-    httpd.write("Content-Type: text/plain\r\n") 
-    httpd.write("Content-Length: 33\r\n\r\n") 
-    httpd.write("REQUEST_URI doesn't match handler") 
+  local env = {}
+  local headers = {}
+  local query = nil 
+  local parts = {}
+  local count = 0
+  local request = nil
+  local request_payload = os.getenv('REQUEST_PAYLOAD')
+  if request_payload ~= nil then
+    request = require(request_payload)
+    env = request.env
+    header = request.headers
+    query = request.query
+  else
+    env.request_uri = os.getenv('REQUEST_URI')
+    env.request_method = os.getenv('REQUEST_METHOD')
+  end
+  if env.request_uri == nil then
+    httpd.print("HTTP/1.1 500 Internal Server Error") 
+    httpd.print("Content-Type: text/plain") 
+    httpd.print("No REQUEST_URI provided") 
+    return 1
+  end
+  for word in string.gmatch(env.request_uri, '([^/]+)') do
+    parts[count] = word
+    count = count + 1
+  end
+  if parts[1] == handler then 
+    return fn(env, headers, query)
+  end
+  httpd.print("HTTP/1.1 404 Not Found") 
+  httpd.print("Content-Type: text/plain") 
+  httpd.print("REQUEST_URI doesn't match handler") 
+  httpd.print("REQUEST_URI=" .. env.request_uri) 
+  httpd.print("handler=" .. handler)
 end
 
 return httpd
